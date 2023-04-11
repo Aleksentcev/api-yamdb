9f068962125eb6e747cb10b91c8887d2679f7fe4
Utigeon <okisay1@gmail.com>
2023-04-11 16:46:51 +0300

    добавлены модели в приложение reviews, users

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,148 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+SCORE_CHOICES = [
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10),
+]
+
+
+class Category(models.Model):
+    category = models.CharField(
+        max_length=40,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        default_related_name = 'categories'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.category
+
+
+class Genre(models.Model):
+    genre = models.CharField(
+        max_length=40,
+        verbose_name='Жанр'
+    )
+
+    class Meta:
+        default_related_name = 'genres'
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.genre
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения'
+    )
+    year = models.IntegerField(
+        max_length=4,
+        verbose_name='Год создания произведения'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Жанр',
+        help_text='Жанр произведения'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категория',
+        help_text='Категория произведения'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        default_related_name = 'titles'
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+    text = models.TextField(
+        'Текст отзыва'
+    )
+    score = models.CharField(
+        max_length=2,
+        choices=SCORE_CHOICES,
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        default_related_name = 'reviews'
+        verbose_name = 'Обзор'
+        verbose_name_plural = 'Обзоры'
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+    )
+    text = models.TextField(
+        'Текст комментария'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        default_related_name = 'comments'
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+ROLE_CHOICES = [
+    ('usr', 'user'),
+    ('mdr', 'moderator'),
+    ('adm', 'admin')
+]
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        max_length=3,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
