0041791d8149deb2b2473ade60eaa483feefce53
Михаил Алексенцев <legolas961@gmail.com>
2023-04-19 21:12:38 +0300

    del import

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, viewsets, mixins, permissions
+from rest_framework import status, viewsets, permissions
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
